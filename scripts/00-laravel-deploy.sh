876dfc773530f9a9f1e36d469220549849351aeb
Eliezer Rangel <86741316+eliezerangel@users.noreply.github.com>
2024-08-09 18:38:53 +0100

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,9 +11,9 @@ echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
-php artisan route:cache
+#php artisan route:cache
 
 echo "Running migrations..."
-#php artisan migrate:fresh
+php artisan migrate:fresh
 
 echo "done deploying"
